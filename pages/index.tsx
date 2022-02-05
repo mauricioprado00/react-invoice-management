8558c35b3bb682d69e3fc8536517550ac7eecaea
Mauricio Prado <mauricioprado00@gmail.com>
2022-02-05 13:26:25 +0200

    repeat the row multiple times

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,39 +31,43 @@ const Home: NextPage = () => {
                   </tr>
                 </thead>
                 <tbody className="text-sm divide-y divide-gray-100">
-                  <tr>
-                    <td className="px-2 py-4 whitespace-nowrap">
-                      <div className="flex items-center">
-                        <div className="flex-shrink-0 h-10 w-10">
-                          <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60" alt="" />
-                        </div>
-                        <div className="ml-4">
-                          <div className="text-sm font-medium text-gray-900">
-                            Jane Cooper
-                          </div>
-                          <div className="text-sm text-gray-500">
-                            jane.cooper@example.com
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="p-2 whitespace-nowrap">
-                      <div className="text-left">Acme</div>
-                    </td>
-                    <td className="p-2 whitespace-nowrap">
-                      <div className="text-left font-medium text-green-500">$2,890.66</div>
-                    </td>
-                    <td className="p-2 whitespace-nowrap">
-                      <div className="text-lg text-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                          <circle cx="12" cy="12" r="1"></circle>
-                          <circle cx="12" cy="19" r="1"></circle>
-                          <circle cx="12" cy="5" r="1"></circle>
-                        </svg>
-                      </div>
-                    </td>
-                  </tr>
+                  {
+                    new Array(10).fill(0).map(i => (
+                        <tr key="nothing">
+                          <td className="px-2 py-4 whitespace-nowrap">
+                            <div className="flex items-center">
+                              <div className="flex-shrink-0 h-10 w-10">
+                                <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60" alt="" />
+                              </div>
+                              <div className="ml-4">
+                                <div className="text-sm font-medium text-gray-900">
+                                  Jane Cooper
+                                </div>
+                                <div className="text-sm text-gray-500">
+                                  jane.cooper@example.com
+                                </div>
+                              </div>
+                            </div>
+                          </td>
+                          <td className="p-2 whitespace-nowrap">
+                            <div className="text-left">Acme</div>
+                          </td>
+                          <td className="p-2 whitespace-nowrap">
+                            <div className="text-left font-medium text-green-500">$2,890.66</div>
+                          </td>
+                          <td className="p-2 whitespace-nowrap">
+                            <div className="text-lg text-center">
+                              <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <circle cx="12" cy="12" r="1"></circle>
+                                <circle cx="12" cy="19" r="1"></circle>
+                                <circle cx="12" cy="5" r="1"></circle>
+                              </svg>
+                            </div>
+                          </td>
+                        </tr>
+                      ))
+                  }
                 </tbody>
               </table>
             </div>
