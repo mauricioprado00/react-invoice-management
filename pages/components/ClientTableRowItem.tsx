159240766bef10e3ed44aa50394685fce023897b
Mauricio Prado <mauricioprado00@gmail.com>
2022-02-05 20:28:27 +0200

    fix build issues

--- pages/components/ClientTableRowItem.tsx
+++ pages/components/ClientTableRowItem.tsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+import Image from 'next/image'
 
 export type CompanyDetails = {
     name: string,
@@ -35,7 +36,7 @@ const ClientTableRowItem = (props: ClientTableRowItemProps) => {
             <td className="px-2 py-4 whitespace-nowrap">
                 <div className="flex items-center">
                     <div className="flex-shrink-0 h-10 w-10">
-                        <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60" alt="" />
+                        <Image className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60" alt="" width={32} height={32} />
                     </div>
                     <div className="ml-4">
                         <div className="text-sm font-medium text-gray-900">
@@ -55,7 +56,7 @@ const ClientTableRowItem = (props: ClientTableRowItemProps) => {
             </td>
             <td className="p-2 whitespace-nowrap">
                 <div className="text-lg text-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer" width="16" height="16" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <circle cx="12" cy="12" r="1"></circle>
                         <circle cx="12" cy="19" r="1"></circle>
