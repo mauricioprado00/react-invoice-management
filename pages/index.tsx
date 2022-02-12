62a5c644a780c0fabeb04ce915710e263e6c76ce
Mauricio Prado <mauricioprado00@gmail.com>
2022-02-12 18:33:30 +0200

    implement change user_id

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import ClientTable from '../components/views/client/ClientTable'
 import InvoiceTable from '../components/views/invoice/InvoiceTable';
 import { generateInvoices } from '../library/lorem-ipsum';
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { ClientListN } from '../models/Client'
 import createApi from './api/apiclient';
 import { InvoiceListN } from '../models/Invoice';
@@ -10,26 +10,26 @@ import { InvoiceListN } from '../models/Invoice';
 let api = createApi('//localhost:3139', '111');
 
 const Home: NextPage = () => {
-  const [counter, setCounter] = useState(0)
+  const [userId, setUserId] = useState('111');
   const [clients, setClients]: [ClientListN, any] = useState(null);
   const [invoices, setInvoices]: [InvoiceListN, any] = useState(null);
+  useMemo(() => {api = createApi('//localhost:3139', userId)}, [userId])
   api.useGetClients((received, abort) => {
-    api = createApi('//localhost:3139', counter % 2 ? '111' : '222')
     setClients(null);
     received(clients => setClients(clients));
     return abort
-  }, [counter]);
+  }, [api]);
 
   api.useGetInvoices((received, abort) => {
     setInvoices(null);
     received(invoices => setInvoices(invoices));
     return abort
-  }, [])
+  }, [api])
 
   return (
     <>
-      counter: {counter}
-      <button onClick={() => setCounter(c => c + 1)}>more</button>
+      UserId: {userId}
+      <button onClick={() => setUserId(userId => userId === '111' ? '222' : '111')}>Change User</button>
       <ClientTable clients={clients} />
       <InvoiceTable invoices={invoices} />
     </>
