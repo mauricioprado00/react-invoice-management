a6af5aa99999a198952d20acd14ecc6759a1ce80
Mauricio Prado <mauricioprado00@gmail.com>
2022-03-04 13:19:18 +0200

    Hard remove routeslice

--- store/RootSlice.ts
+++ store/RootSlice.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from "redux"
 import entitiesReducer from './EntitiesSlice'
-import RouteSlice from "./RouteSlice";
 import userReducer from './UserSlice'
 
 const rootReducer = combineReducers({
 	entities: entitiesReducer,
 	user: userReducer,
-	route: RouteSlice
 });
 
 export type RootState = ReturnType<typeof rootReducer>
