7f7f8721c424f468fc68d467a89cb990557143e4
Mauricio Prado <mauricioprado00@gmail.com>
2022-03-08 23:57:00 +0200

    Change profile edit icon

--- components/views/profile/ProfileCard.tsx
+++ components/views/profile/ProfileCard.tsx
@@ -4,6 +4,8 @@ import { AnyClient, AnyClientPropTypes } from 'models/Client'
 import { Me, MePropTypes } from 'models/User'
 import { getAvatarImageUrl } from '../../ui/forms/AvatarSelector'
 import CompanyInfo from '../company/CompanyInfo'
+import { Fab } from '@mui/material'
+import EditIcon from '@mui/icons-material/Edit';
 
 type ProfileCardProps = {
     profile: AnyClient | Me,
@@ -34,9 +36,10 @@ function ProfileCard({ children, profile, onEdit }: ProfileCardProps) {
                     <h2 className="text-gray-800 text-3xl font-bold">{name}</h2>
                     <p className="text-gray-400 mt-2">{email}</p>
                     {companyDetails && <CompanyInfo companyDetails={companyDetails} />}
-                    {onEdit && <div className="absolute bottom-px right-2.5 cursor-pointer" onClick={onEdit}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="text-gray-700" fill="currentColor" viewBox="0 0 24 24" width={24} height={24}><path fill="none" d="M0 0h24v24H0z" /><path d="M9.243 19H21v2H3v-4.243l9.9-9.9 4.242 4.244L9.242 19zm5.07-13.556l2.122-2.122a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414l-2.122 2.121-4.242-4.242z" /></svg>
-                    </div>}
+                    {onEdit && <Fab size="small" color="secondary" aria-label="edit" onClick={onEdit}>
+                        <EditIcon />
+                    </Fab>
+                    }
                 </div>
                 <hr className="mt-6" />
                 <div className="flex  bg-gray-50 ">
