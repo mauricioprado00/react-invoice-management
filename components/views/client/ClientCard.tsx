a90cc5941ea89a3bb719e06109b749dbeeeb9e8b
Mauricio Prado <mauricioprado00@gmail.com>
2022-03-19 14:32:05 +0200

    Fix totals in ClientCard

--- components/views/client/ClientCard.tsx
+++ components/views/client/ClientCard.tsx
@@ -16,11 +16,11 @@ function ClientCard({ client, onEdit }: ClientCardProps) {
     return (
         <ProfileCard profile={client} onEdit={onEdit}>
             <div className="text-center w-1/2 p-4 hover:bg-gray-100">
-                <p><span className="font-semibold">${client.totalBilled}</span> Total Billed</p>
+                <p><span className="font-semibold">${client.totalBilled.toFixed(2)}</span> Total Billed</p>
             </div>
             <div className="border" />
             <div className="text-center w-1/2 p-4 hover:bg-gray-100">
-                <p> <span className="font-semibold">{20} </span> Invoices</p>
+                <p> <span className="font-semibold">{client.invoicesCount ? client.invoicesCount + ' Invoices' : 'No invoices yet'} </span></p>
             </div>
         </ProfileCard>
     )
