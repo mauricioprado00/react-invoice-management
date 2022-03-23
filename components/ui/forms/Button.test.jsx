6d476a7c9f728c3c1d3cc79a58505e757d914df8
Mauricio Prado <mauricioprado00@gmail.com>
2022-03-24 00:01:16 +0200

    add test for Button

---
+++ components/ui/forms/Button.test.jsx
@@ -0,0 +1,20 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import Button from "./Button";
+
+describe("Button", () => {
+  it("reanders the child nodes", () => {
+    render(<Button>Some content</Button>);
+    const element = screen.getByText(/Some content/i);
+    expect(element).toBeInTheDocument();
+  });
+  it("will forward the onClick event", () => {
+    let clicked = false;
+    const clickHandler = () => {
+      clicked = true;
+    };
+    render(<Button onClick={clickHandler}>Some content</Button>);
+    fireEvent.click(screen.getByText("Some content"));
+    expect(clicked).toBe(true);
+  });
+});
