961ca7966dff40bf9e2a6f0ae8cf33762dbc531b
Mauricio Prado <mauricioprado00@gmail.com>
2022-03-24 11:49:27 +0200

    add new test to check that users without completed profile will be redirected to profile edition page

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,5 +1,6 @@
 
 describe("User login", () => {
+  
   it("should navigate to the dashboard when profile is completed", () => {
     cy.visit("http://localhost:3000/login");
 
@@ -8,6 +9,16 @@ describe("User login", () => {
     cy.get('button').contains('Sign In').click();
     cy.url().should("equal", "http://localhost:3000/");
     cy.get("h2").contains("Latest Clients");
+  });
+
+  it("should navigate to the profile edition when profile is not completed", () => {
+    cy.visit("http://localhost:3000/login");
+
+    cy.get('input[name="email"]').click().type("user-without-completed-profile@officehourtesting.com");
+    cy.get('input[name="password"]').click().type("123456");
+    cy.get('button').contains('Sign In').click();
+    cy.url().should("equal", "http://localhost:3000/update-profile");
+    cy.get("h2").contains("Profile Edition");
   });
 });
 
